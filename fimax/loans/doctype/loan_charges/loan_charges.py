d055e271a09396e1bedd80b1d17fc0123ac9c97e
Yefri Tavarez <yefritavarez@hotmail.com>
2018-04-05 02:20:22 -0400

    Add some validations to Loan Charges doctype

--- fimax/loans/doctype/loan_charges/loan_charges.py
+++ fimax/loans/doctype/loan_charges/loan_charges.py
@@ -11,7 +11,8 @@ from frappe import _ as __
 
 class LoanCharges(Document):
 	def validate(self):
-		pass
+		self.validate_amounts()
+		self.set_missing_values()
 
 	def before_insert(self):
 		pass
@@ -23,7 +24,7 @@ class LoanCharges(Document):
 		pass
 
 	def on_submit(self):
-		pass
+		self.update_outstanding_amount()
 
 	def before_cancel(self):
 		pass
@@ -33,3 +34,45 @@ class LoanCharges(Document):
 
 	def on_trash(self):
 		pass
+
+	def set_missing_values(self):
+		self.total_amount = flt(self.amount) + flt(self.fine_amount)
+		self.outstanding_amount = self.total_amount
+
+	def update_outstanding_amount(self):
+		if not self.docstatus == 1.000:
+			frappe.throw(__("Please submit this Loan Charge before updating the outstanding amount!"))
+			
+		outstanding_amount = flt(self.total_amount) - flt(self.paid_amount)
+
+		if outstanding_amount < 0.000:
+			frappe.throw(__("Outstanding amount cannot be negative!"))
+
+		self.outstanding_amount = outstanding_amount
+			
+
+	def validate_amounts(self):
+		if not flt(self.amount):
+			frappe.throw(__("Missing amount!"))
+
+		if flt(self.paid_amount) > flt(self.total_amount):
+			frappe.throw(__("Paid Amount cannot be greater than Total amount!"))
+			
+
+	def update_status(self):
+
+		# it's pending if repayment date is in the future and has nothing paid
+		if cstr(self.repayment_date) >= nowdate() and self.paid_amount == 0.000:
+			self.status = "Pending"
+
+		# it's partially paid if repayment date is in the future and has something paid
+		if cstr(self.repayment_date) > nowdate() and self.paid_amount > 0.000:
+			self.status = "Partially"
+
+		# it's overdue if repayment date is in the past and is not fully paid
+		if cstr(self.repayment_date) < nowdate() and self.outstanding_amount > 0.000:
+			self.status = "Overdue"
+
+		# it's paid if paid and total amount are equal hence there's not outstanding amount
+		if self.paid_amount == self.total_amount:
+			self.status = "Paid"
