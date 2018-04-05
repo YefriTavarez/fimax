99c167722c2bcab672b80f46af92223ae24571e8
Yefri Tavarez <yefritavarez@hotmail.com>
2018-04-04 22:11:44 -0400

    Add boilerplate for loan_charges's python file

--- fimax/loans/doctype/loan_charges/loan_charges.py
+++ fimax/loans/doctype/loan_charges/loan_charges.py
@@ -6,5 +6,30 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
+from frappe.utils import flt, cint, cstr, nowdate
+from frappe import _ as __
+
 class LoanCharges(Document):
-	pass
+	def validate(self):
+		pass
+
+	def before_insert(self):
+		pass
+
+	def after_insert(self):
+		pass
+
+	def before_submit(self):
+		pass
+
+	def on_submit(self):
+		pass
+
+	def before_cancel(self):
+		pass
+
+	def on_cancel(self):
+		pass
+
+	def on_trash(self):
+		pass
