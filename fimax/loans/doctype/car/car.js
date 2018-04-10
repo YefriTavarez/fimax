213256428adfe2ec1af1afa5e664e62d277dce20
Yefri Tavarez <yefritavarez@hotmail.com>
2018-04-10 10:01:13 -0400

    When you create a new Vehicle or Property from Loan has to go back to the original URL Issue: #1

--- fimax/loans/doctype/car/car.js
+++ fimax/loans/doctype/car/car.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2017, Yefri Tavarez and contributors
 // For license information, please see license.txt
 
+frappe.provide("fimax.loan");
 frappe.ui.form.on('Car', {
-	"refresh": (frm) => {
+	"after_save": (frm) => {
+		let url = fimax.loan.url;
 
+		if (!url) {
+			return 0;
+		}
+
+		frappe.model.set_value(url[1], url[2], "asset_type", frm.doctype);
+		frappe.model.set_value(url[1], url[2], "asset", frm.docname);
+
+		setTimeout(() => frappe.set_route(url), 500);
+
+		delete fimax.loan.url;
 	}
 });
--- fimax/loans/doctype/property/property.js
+++ fimax/loans/doctype/property/property.js
@@ -1,23 +1,38 @@
 // Copyright (c) 2017, Yefri Tavarez and contributors
 // For license information, please see license.txt
 
+frappe.provide("fimax.loan");
 frappe.ui.form.on('Property', {
-	"refresh": (frm) => {
+	"after_save": (frm) => {
+		let url = fimax.loan.url;
 
+		if (!url) {
+			return 0;
+		}
+
+		frappe.model.set_value(url[1], url[2], "asset_type", frm.doctype);
+		frappe.model.set_value(url[1], url[2], "asset", frm.docname);
+
+		setTimeout(() => frappe.set_route(url), 500);
+
+		delete fimax.loan.url;
 	},
 	"construction_year": (frm) => {
-		if (!frm.doc.construction_year)
-			return;
-
-		this_year = eval(frappe.datetime.get_today().split("-")[0]);
-		
-		if(frm.doc.construction_year <= this_year){
-			frm.set_value("construction_age", this_year - frm.doc.construction_year);
+		if (!frm.doc.construction_year) {
+			return 0;
 		}
-		else{
-			frm.set_value("construction_age", undefined);
-			frm.set_value("construction_year", undefined);
-			frappe.show_alert(__("Construction year must be </br> greater or equal this year!"),5);
+
+		let current_year = cint(frappe.datetime.get_today().split("-")[0]);
+		
+		if (frm.doc.construction_year <= current_year){
+			let construction_age = current_year - frm.doc.construction_year;
+			frm.set_value("construction_age", construction_age);
+		} else{
+			frappe.run_serially([
+				() => frm.set_value("construction_age", undefined),
+				() => frm.set_value("construction_year", undefined),
+				() => frappe.throw(__("Construction year must be </br> greater or equal this year!"))
+			]);
 		}
 	}
 });
