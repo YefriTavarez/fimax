a03980541bf25df7f1ddc0fc41c69f1a1a195188
Yefri Tavarez <yefritavarez@hotmail.com>
2018-04-12 18:50:15 -0400

    Now each Loan Repayment Schedule can help with the Loan Charge Creation

--- fimax/loans/doctype/loan_repayment_schedule/loan_repayment_schedule.py
+++ fimax/loans/doctype/loan_repayment_schedule/loan_repayment_schedule.py
@@ -7,4 +7,26 @@ import frappe
 from frappe.model.document import Document
 
 class LoanRepaymentSchedule(Document):
-	pass
+	def get_new_loan_charge(self, loan_charges_type, amount):
+		return frappe.get_doc({
+			'amount': amount,
+			'doctype': 'Loan Charges',
+			'loan_charges_type': loan_charges_type,
+			'outstanding_amount': amount,
+			'paid_amount': 0.000,
+			'reference_type': self.doctype,
+			'reference_name': self.name,
+			'loan': self.parent,
+			'repayment_date': self.repayment_date,
+			'status': 'Pending',
+			'total_amount': amount
+		})
+
+	def get_loan_charge(self, loan_charges_type):
+		return frappe.get_doc("Loan Charge", {
+			'repayment_date': self.repayment_date,
+			'loan': self.parent,
+			'reference_type': self.doctype,
+			'reference_name': self.name,
+			'loan_charges_type': loan_charges_type,
+		})
