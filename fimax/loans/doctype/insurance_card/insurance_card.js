ce628d0ea86618b9cec69a718ede49b15e47da11
Lewin Villar <lewin.villar@gmail.com>
2018-05-31 21:21:18 -0400

    Some finishes to Insurance Card

--- fimax/loans/doctype/insurance_card/insurance_card.js
+++ fimax/loans/doctype/insurance_card/insurance_card.js
@@ -9,18 +9,26 @@ frappe.ui.form.on('Insurance Card', {
 			frm.trigger("start_date");
 		}
 
-		let fields = ["party", "party_type", "party_name", "company"];
-		$.map(fields, (field)=> frm.add_fetch("loan", field, field));
+		$.each({
+			"party": "party",
+			"party_type": "party_type",
+			"party_name": "party_name",
+			"company": "company",
+			"company_currency": "company_currency",
+			"currency": "currency"
+		}, (key, value)=> frm.add_fetch("loan", key, value));
+
+		frm.trigger("set_status_indicators");
 	},
 	"start_date": (frm) => {
 		frm.set_value("end_date", frappe.datetime.add_months(frm.doc.start_date, 12));
 	},
 	"total_amount": (frm) => {
-		events = ["calculate_pending_amount", "make_repayment_schedule"];
+		events = ["calculate_outstanding_amount", "make_repayment_schedule"];
 		$.map(events, (event) => frm.trigger(event));
 	},
 	"initial_payment_amount": (frm) => {
-		frm.trigger("calculate_pending_amount");
+		frm.trigger("calculate_outstanding_amount");
 		if (frm.doc.initial_payment_amount < 0.00) {
 			let a_third_of_total_amt = frm.doc.total_amount * 0.3;
 			
@@ -29,13 +37,13 @@ frappe.ui.form.on('Insurance Card', {
 		}
 		frm.trigger("make_repayment_schedule");
 	},
-	"calculate_pending_amount": (frm) => {
+	"calculate_outstanding_amount": (frm) => {
 		if (frm.doc.initial_payment_amount > frm.doc.total_amount) {
 			frappe.msgprint(__("Initial Payment Amount can't be greater that Total Amount!"));
 		}
 			
-		let pending_amount = frm.doc.total_amount - frm.doc.initial_payment_amount;
-		frm.set_value("pending_amount", pending_amount);
+		let outstanding_amount = frm.doc.total_amount - frm.doc.initial_payment_amount;
+		frm.set_value("outstanding_amount", outstanding_amount);
 	},
 	"repayment_periods": (frm) => {
 		let events = ["validate_repayment_periods", "make_repayment_schedule"];
@@ -54,32 +62,58 @@ frappe.ui.form.on('Insurance Card', {
 	"make_repayment_schedule": (frm) => {
 		if (frm.doc.total_amount <= 0.00 || frm.doc.is_paid_upfront) { return ; }
 
-		let monthly_insurance_amount = cint(frm.doc.pending_amount / frm.doc.repayment_periods);
-		let pending_amount = frm.doc.pending_amount;
+		let monthly_repayment_amount = cint(frm.doc.outstanding_amount / frm.doc.repayment_periods);
+		let outstanding_amount = frm.doc.outstanding_amount;
 		let allocated_amount = 0.00;
 		let row_idx = 1;
 
 		frm.doc.insurance_repayment_schedule = [];
 		let row;
 
-		while (pending_amount >= monthly_insurance_amount) {
+		while (outstanding_amount >= monthly_repayment_amount) {
 			row = frm.add_child("insurance_repayment_schedule", {
 				"repayment_date": frappe.datetime.add_months(frm.doc.start_date, row_idx),
 				"paid_amount": 0.00,
-				"insurance_amount": monthly_insurance_amount,
-				"pending_amount": monthly_insurance_amount,
+				"repayment_amount": monthly_repayment_amount,
+				"outstanding_amount": monthly_repayment_amount,
 				"status": "Pending"
 			});
 
-			pending_amount -= monthly_insurance_amount;
-			allocated_amount += monthly_insurance_amount;
+			outstanding_amount -= monthly_repayment_amount;
+			allocated_amount += monthly_repayment_amount;
 			row_idx ++;
 		}
 
-		real_pending_amount = frm.doc.pending_amount - allocated_amount;
-		row.insurance_amount += real_pending_amount;
-		row.pending_amount += real_pending_amount;
+		real_outstanding_amount = frm.doc.outstanding_amount - allocated_amount;
+		row.repayment_amount += real_outstanding_amount;
+		row.outstanding_amount += real_outstanding_amount;
 
+		frm.trigger("set_status_indicators");
 		frm.refresh_fields();
-	}
+	},
+	"set_status_indicators": (frm) => {
+		let grid = frm.get_field('insurance_repayment_schedule').grid;
+
+		grid.wrapper.find("div.rows .grid-row").each((idx, vhtml) => {
+			let doc = frm.doc.insurance_repayment_schedule[idx];
+
+			let html = $(vhtml);
+
+			let indicator = {
+				"Pending": "indicator orange",
+				"Overdue": "indicator red",
+				"Partially Paid": "indicator yellow",
+				"Paid": "indicator green",
+			}[doc.status];
+
+			// let's remove any previous set class
+			$.map(["orange", "red", "yellow", "green"], (color) => {
+				html.find("div[data-fieldname=status] .static-area.ellipsis")
+					.removeClass(__("indicator {0}", [color]));
+			});
+
+			html.find("div[data-fieldname=status] .static-area.ellipsis").addClass(indicator);
+		});
+	},
+
 });
--- fimax/loans/doctype/insurance_repayment_schedule/insurance_repayment_schedule.py
+++ fimax/loans/doctype/insurance_repayment_schedule/insurance_repayment_schedule.py
@@ -7,4 +7,37 @@ import frappe
 from frappe.model.document import Document
 
 class InsuranceRepaymentSchedule(Document):
-	pass
+	
+	def rename(self):
+		new_name = "{0}-NO-{1}".format(self.parent, self.idx)
+		frappe.rename_doc(self.doctype, self.name, new_name, force=True)
+
+	def get_new_loan_charge(self, loan_charges_type, amount):
+		insurance_card = frappe.get_doc(self.parenttype, self.parent)
+
+		return frappe.get_doc({
+			'doctype': 'Loan Charges',
+			'loan_charges_type': loan_charges_type,
+			'outstanding_amount': amount,
+			'paid_amount': 0.000,
+			'reference_type': self.doctype,
+			'reference_name': self.name,
+			'loan': insurance_card.loan,
+			'repayment_date': self.repayment_date,
+			'status': 'Pending',
+			'repayment_period': self.idx,
+			'total_amount': amount
+		})
+
+	def get_loan_charge(self, loan_charges_type):
+		insurance_card = frappe.get_doc(self.parenttype, self.parent)
+		
+		filters_dict = {
+			'loan_charges_type': loan_charges_type,
+			'repayment_date': cstr(self.repayment_date),
+			'loan': insurance_card.loan,
+			'reference_type': self.doctype,
+			'reference_name': self.name,
+		}
+
+		return frappe.get_all("Loan Charges", filters_dict)
