521bc31721d428458b454fec2780a0357a566e40
Yefri Tavarez <yefritavarez@hotmail.com>
2018-06-13 20:33:54 -0400

    Fix bug that was reloading the page twice when the user changed the theme and the language a the same time

--- fimax/public/js/user.js
+++ fimax/public/js/user.js
@@ -1,5 +1,12 @@
 frappe.ui.form.on("User", {
 	"refresh": (frm) => {
+		if (frm.doc.name===frappe.session.user && !frm.doc.__unsaved
+			&& frappe.all_timezones
+			&& (frm.doc.language || frappe.boot.user.language)
+			&& frm.doc.language !== frappe.boot.user.language) {
+			return ; // as this condition is true there no need for reload from here
+		}
+
 		if(frm.doc.name == frappe.session.user && !frm.doc.__unsaved
 			&& (frm.doc.dark_theme || frappe.boot.user.defaults.dark_theme)
 			&& frm.doc.dark_theme !== frappe.boot.user.defaults.dark_theme) {
