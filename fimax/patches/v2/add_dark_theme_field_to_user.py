ef6a86e0173396759e9b8d5ba38b8ab649be7941
Yefri Tavarez <yefritavarez@hotmail.com>
2018-06-14 00:39:33 -0400

    Add patch for custom field dark theme in User doctype

---
+++ fimax/patches/v2/add_dark_theme_field_to_user.py
@@ -0,0 +1,45 @@
+import frappe
+
+def execute():
+	if frappe.db.exists("Custom Field", 
+		"User-dark_theme"): return
+
+	frappe.get_doc({
+		'allow_on_submit': 0,
+		'bold': 0,
+		'collapsible': 0,
+		'collapsible_depends_on': None,
+		'columns': 0,
+		'default': '1',
+		'depends_on': None,
+		'description': None,
+		'doctype': 'Custom Field',
+		'dt': 'User',
+		'fieldname': 'dark_theme',
+		'fieldtype': 'Check',
+		'hidden': 0,
+		'idx': 15,
+		'ignore_user_permissions': 0,
+		'ignore_xss_filter': 0,
+		'in_global_search': 0,
+		'in_list_view': 0,
+		'in_standard_filter': 0,
+		'insert_after': 'user_image',
+		'label': 'Use Dark Theme',
+		'no_copy': 0,
+		'options': None,
+		'parent': None,
+		'parentfield': None,
+		'parenttype': None,
+		'permlevel': 0,
+		'precision': '',
+		'print_hide': 0,
+		'print_hide_if_no_value': 0,
+		'print_width': None,
+		'read_only': 0,
+		'report_hide': 0,
+		'reqd': 0,
+		'search_index': 0,
+		'unique': 0,
+		'width': None
+	}).insert(ignore_permission=True)
