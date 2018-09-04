e55c8e0f40c95af4d0a6b0bed23e1c8c432b3f43
Manuel Hiciano <m_hiciano@hotmail.com>
2018-09-03 23:13:46 -0400

    Fix Patch to GPS Installation doctype

--- fimax/patches/v2/drop_column_starts_date_in_gps_form.py
+++ fimax/patches/v2/drop_column_starts_date_in_gps_form.py
@@ -2,6 +2,10 @@ import frappe
 
 def execute():
 	try:
+		frappe.db.sql("update `tabGPS Installation` set start_date = starts_date where start_date is null")
+	except: pass
+
+	try:
 		frappe.db.sql("alter table `tabGPS Installation` drop column starts_date")
-	except:
-		pass
+	except: pass
+
