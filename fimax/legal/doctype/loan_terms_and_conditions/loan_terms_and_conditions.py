b955ca05d117b99fc3ad5a146865cc88d4a63927
Yefri Tavarez <yefri@soldeva.com>
2018-09-05 00:12:52 -0400

    Update Loan Terms and Conditions files

--- fimax/legal/doctype/loan_terms_and_conditions/loan_terms_and_conditions.py
+++ fimax/legal/doctype/loan_terms_and_conditions/loan_terms_and_conditions.py
@@ -7,4 +7,15 @@ import frappe
 from frappe.model.document import Document
 
 class LoanTermsandConditions(Document):
-	pass
+	def autoname(self):
+		self.name = "{0}-{1}".format(self.loan, 
+			self.terms_template)
+
+	def get_terms_and_conditions(self):
+		template = frappe.get_value(self.meta.get_field("terms_template").options, 
+			self.terms_template, "terms")
+
+		doc = frappe.get_doc(self.meta.get_field("loan").options,
+			self.loan)
+
+		self.terms_and_conditions = frappe.render_template(template, doc.as_dict())
