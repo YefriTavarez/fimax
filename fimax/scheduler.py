b6b2fd3396b0758520095d865c7860a39ead2993
Yefri Tavarez <yefritavarez@hotmail.com>
2019-03-17 12:56:27 -0400

    [Bug Fix] When sheduler run it throws and error when dealing with unpaid Insurance Card - Loan Records

--- fimax/scheduler.py
+++ fimax/scheduler.py
@@ -1,8 +1,9 @@
 import frappe
 
-from frappe import _
 from frappe.utils import cint, flt, cstr
 from frappe.utils import nowdate, add_days, add_months
+
+from frappe import get_doc, db, _
 
 def all():
 	pass
@@ -10,7 +11,7 @@ def all():
 def daily():
 	update_status_to_loan_charges()
 	create_loan_charges_fines()
-	
+
 def hourly():
 	pass
 
@@ -23,15 +24,15 @@ def monthly():
 def update_status_to_loan_charges():
 	"""Update status for each Loan Charge based on the current date and the paid amount"""
 	for doc in get_valid_loan_charges():
-		if not frappe.db.exists(doc.doctype, 
+		if not db.exists(doc.doctype,
 			doc.name): return
 
 		# it exists, so then let's get it
-		doc = frappe.get_doc(doc.doctype, doc.name)
+		doc = get_doc(doc.doctype, doc.name)
 
-		doc.update_references(cancel=False)
+		doc.run_method("update_references", cancel=False)
 
-		doc.update_status()
+		doc.run_method("update_status")
 
 		# submit to update the database
 		doc.submit()
@@ -39,18 +40,18 @@ def update_status_to_loan_charges():
 def create_loan_charges_fines():
 	for company, in frappe.get_list("Company", as_list=True):
 		create_loan_charges_fines_for_(company)
-			
+
 def create_loan_charges_fines_for_(company):
-	if not frappe.db.exists("Company Defaults", {
+	if not db.exists("Company Defaults", {
 		"company": company
 	}): return
 
-	company_defaults = frappe.get_doc("Company Defaults", {
+	company_defaults = get_doc("Company Defaults", {
 		"company": company
 	}).as_dict()
 
 	today = nowdate()
-	
+
 	due_date = today
 
 	if company_defaults.grace_days:
@@ -67,17 +68,17 @@ def create_loan_charges_fines_for_(company):
 		"repayment_date": ["<=", due_date]
 	}, as_list=True):
 
-		reference_type, reference_name = frappe.get_value(doctype, name, 
+		reference_type, reference_name = frappe.get_value(doctype, name,
 			["reference_type", "reference_name"])
 
-		total_amount, outstanding_amount = frappe.get_value(doctype, name, 
+		total_amount, outstanding_amount = frappe.get_value(doctype, name,
 			["total_amount", "outstanding_amount"])
-		
-		loan_repayment = frappe.get_doc(reference_type, reference_name)
+
+		loan_repayment = get_doc(reference_type, reference_name)
 
 		time_to_run = None
-		
-		if loan_repayment.last_run:
+
+		if loan_repayment.get("last_run"):
 			time_to_run = add_months(loan_repayment.last_run, 1)
 
 		if time_to_run and not cstr(time_to_run) <= nowdate():
@@ -86,29 +87,32 @@ def create_loan_charges_fines_for_(company):
 		amount = late_payment_fee_rate * total_amount if late_payment_fee_on_total_amount \
 			else outstanding_amount
 
-		loan_charges = loan_repayment.get_new_loan_charge("Late Payment Fee", amount)
+		if hasattr(loan_repayment, "get_new_loan_charge"):
+			loan_charges = loan_repayment \
+				.get_new_loan_charge("Late Payment Fee",
+									 amount)
 
 		if not amount: continue
-		
+
 		loan_repayment.last_run = nowdate()
 
 		loan_repayment.db_update()
 
 		if loan_repayment.parenttype == "Loan":
 			update_loan_record(
-				frappe.get_doc(loan_repayment.parenttype, 
+				get_doc(loan_repayment.parenttype,
 					loan_repayment.parent))
 
 		loan_charges.submit()
 
 def get_valid_loan_charges():
-	return frappe.db.sql("""SELECT 
-			loan_charges.name AS name, 
+	return db.sql("""SELECT
+			loan_charges.name AS name,
 			'Loan Charges' AS doctype
 		FROM
 			`tabLoan Charges` AS loan_charges
 		INNER JOIN
-			`tabLoan Charges Type` AS loan_charges_type 
+			`tabLoan Charges Type` AS loan_charges_type
 			ON loan_charges.loan_charges_type = loan_charges_type.name
 		WHERE loan_charges_type.generates_fine > 0
 			AND TIMESTAMPDIFF(MONTH, loan_charges.modified, CURRENT_TIMESTAMP) > 0
@@ -119,18 +123,18 @@ def get_valid_loan_charges():
 def update_loan_record(doc):
 	tbody, doc = [], get_loan_record(doc)
 
-	doc.details = frappe.render_template("templates/loan_record_details.html", { 
-		"rows": frappe.get_doc(doc.meta.get_field("loan").options, doc.name).get("loan_schedule")
+	doc.details = frappe.render_template("templates/loan_record_details.html", {
+		"rows": get_doc(doc.meta.get_field("loan").options, doc.name).get("loan_schedule")
 	})
 
   	doc.save()
-	
+
 def get_loan_record(doc):
 	import fimax.utils
 
 	doctype = "Loan Record"
 
-	if not frappe.db.exists(doctype, doc.name):
+	if not db.exists(doctype, doc.name):
 		return fimax.utils.create_loan_record(doc)
 
-	return frappe.get_doc(doctype, doc.name)
+	return get_doc(doctype, doc.name)
