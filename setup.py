6e62f4dd7b56ba8ea8b0f16682aaf012d8f39906
Yefri Tavarez <yefri@soldeva.com>
2019-05-06 23:39:44 -0400

    Add Support to V11

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 import re, ast
+
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in fimax/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('fimax/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
-
-requirements = parse_requirements("requirements.txt", session="")
+	version = str(ast.literal_eval(_version_re.search(
+		f.read().decode('utf-8')).group(1)))
 
 setup(
 	name='fimax',
 	version=version,
-	description='Manage your finances with the best manager',
-	author='Yefri Tavarez',
-	author_email='yefritavarez@gmail.com',
+	description='A Loan App',
+	author='Lewin Villar',
+	author_email='lewin.villar@gmail.com',
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
 )
+
